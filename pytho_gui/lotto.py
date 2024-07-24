e0663ade4bcbc19685f72b896de124d3c32356a1
nick <rkd7385@naver.com>
2024-07-24 12:58:28 +0900

    lotto 수정

--- pytho_gui/lotto.py
+++ pytho_gui/lotto.py
@@ -1,13 +1,13 @@
 # 로또 모듈
 import random
-# input : x, output:(로또 번호)
+# input : x, output: set(로또 번호)
 def make_lotto():
     user_lotto = set()
     while len(user_lotto) < 6:
         number = random.randint(1, 45)
         user_lotto.add(number)
     return user_lotto
-# input : (생성 수), out : list(로또 리스트)
+# input: n (생성 수), output : list(로또 리스트)
 def lotto_maker(n):
     lotto_list = []
     for i in range(n):
